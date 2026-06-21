e7483e2563aba5d50bac4575838fda59e58bb3a7
ab-dauletkhan <ab.dauletkhan@gmail.com>
2026-06-21 18:20:22 +0500

    fifth

--- kiim_shop/settings.py
+++ kiim_shop/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "kiim_shop.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": Path(os.getenv("SQLITE_DATABASE_PATH", BASE_DIR / "db.sqlite3")),
     }
 }
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 exec gunicorn kiim_shop.wsgi:application --bind 0.0.0.0:${PORT:-8000}
